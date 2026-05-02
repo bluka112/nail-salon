de68ae234b0fe55676f6e7da713d43c2c6ee5d2a
Luka <blukaCode@gmail.com>
2026-05-01 21:54:18 -0500

    second

---
+++ app/api/branch/[id]/route.ts
@@ -0,0 +1,41 @@
+import { prisma } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export const DELETE = async (
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) => {
+  const { isAuthenticated } = await auth();
+  if (!isAuthenticated) {
+    return NextResponse.json({ message: "you are not authenticated" });
+  }
+  const { id } = await params;
+  const find = prisma.branch.findUnique({ where: { id } });
+  if (find == null) {
+    return NextResponse.json({ message: "not found" });
+  }
+  await prisma.branch.delete({ where: { id } });
+  return NextResponse.json(find);
+};
+
+export const PATCH = async (
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) => {
+  const { isAuthenticated } = await auth();
+  if (!isAuthenticated) {
+    return NextResponse.json({ message: "you are not authenticated" });
+  }
+  const { id } = await params;
+  const body = await req.json();
+  const find = await prisma.branch.findUnique({ where: { id } });
+  if (find == null) {
+    return NextResponse.json({ message: "not found" });
+  }
+  const result = await prisma.branch.update({
+    where: { id },
+    data: body,
+  });
+  return NextResponse.json(result);
+};
